7d5e6e65349389aaf5174961c764e05c97194648
Brian Coria <brian_coria@outlook.com>
2019-12-03 12:06:02 -0400

    dia_2 de la mañana

---
+++ dia_2/vue.js
@@ -0,0 +1,47 @@
+new Vue ({
+    el:'.app',
+    data: {
+        mensaje: 'hola, soy brian',
+        mostrar:true,
+        mostrarFrutas:false,
+        mostrar1: false,
+        frutas:[
+            {
+                nombre: 'papaya',
+                precio: 5,
+                color: 'anaranjado'
+            },
+            {
+                nombre: 'banana',
+                precio: 1,
+                color: 'amarillo'
+            },
+            {
+                nombre: 'manzana',
+                precio: 5,
+                color: 'verde'
+            },
+            {
+                nombre: 'frutilla',
+                precio: 4,
+                color: 'rojo'
+            },
+            {
+                nombre: 'coco',
+                precio: 15,
+                color: 'marron'
+            },
+            {
+                nombre: 'uva',
+                precio: 6,
+                color: 'marron'
+            },
+            {
+                nombre: 'mandarina',
+                precio: 2,
+                color: 'marron'
+            },
+
+        ]
+    }    
+})
