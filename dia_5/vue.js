6bce3d88ac559ab27913ba2ce1feb099203309c2
Brian Coria <brian_coria@outlook.com>
2019-12-09 12:24:34 -0400

    Terminando la app de lista de tareas y realizando otro app de lista de frutas

--- dia_5/vue.js
+++ dia_5/vue.js
@@ -1,20 +1,39 @@
 new Vue ({
     el: "#app",
     data:{
-        agregar_tareas:'',
-        lista_tareas:[],
-        activo:false
+       
+        lista_tareas:[  {
+            tareas: "Tarea Ejemplo",
+            estado:false
+        }],
+        
+        
         
        
         
         
     },
     methods:{
-       agregarTarea(agregar_tareas){
-           this.lista_tareas.push(agregar_tareas)
+       agregarTarea(){
+           if (this.agregar_tareas) {
+            
+               this.lista_tareas.push(
+                   {
+
+                       tareas: this.agregar_tareas,
+                       estado:false
+                   }
+               )        
+           }
+           else {
+            alert("No esta ingresando tarea!!!!!!!!!!!!")
+           }
+           
+           this.agregar_tareas= "";
        },
+
        borrarTarea(index){
-           this.lista_tareas.splice(index, 1 )
+           this.lista_tareas.splice(index, 1)
        }
        
        
---
+++ dia_6/vue.js
@@ -0,0 +1,24 @@
+new Vue({
+    el:"#app",
+    data:{
+        frutas:[
+            {nombre:"Uva", cantidad: 2},
+            {nombre:"Banana", cantidad: 12},
+            {nombre:"Pera", cantidad: 1},
+            {nombre:"Sandia", cantidad:23}
+           
+
+            
+        ]
+        
+    },
+    methods: {
+      
+        
+    }
+
+
+
+
+
+})
