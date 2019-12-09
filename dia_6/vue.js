7f6235e0d10755335996c3e0743378b2f64dad6c
Brian Coria <brian_coria@outlook.com>
2019-12-09 17:44:23 -0400

    Realizando tarea de la mañana con Fructoso agregando estilos + - y el total

--- dia_6/vue.js
+++ dia_6/vue.js
@@ -3,19 +3,59 @@ new Vue({
     data:{
         frutas:[
             {nombre:"Uva", cantidad: 2},
-            {nombre:"Banana", cantidad: 12},
-            {nombre:"Pera", cantidad: 1},
+            {nombre:"Limon", cantidad: 12},
+            {nombre:"Pera", cantidad: 7},
             {nombre:"Sandia", cantidad:23}
            
 
             
         ]
+       
         
     },
     methods: {
-      
-        
+        total(){
+            auxiliar = 0 ;
+            for(l of this.frutas){
+                 auxiliar = auxiliar + l.cantidad;
+
+            }
+            return auxiliar
+        },
+        agregarFruta(){
+            if (this.nuevaFruta) {
+             
+                this.frutas.push(
+                    {
+ 
+                        nombre: this.nuevaFruta,
+                        cantidad:0
+                    }
+                )        
+            }
+            else {
+             alert("No esta ingresando una Fruta!!!!!!!!!!!!")
+            }
+            
+            this.nuevaFruta= "";
+        },
+        borrarFruta(index){
+            this.frutas.splice(index, 1)
+        },
+        cambiarColor(l){
+            if (l.cantidad <=5) {
+                
+                return 'badge1'
+            }
+            if (l.cantidad >=6) {
+                return 'badge2'
+            }
+            
+            
+        }
     }
+        
+    
 
 
 
