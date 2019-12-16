0b04ff796300320e73f118b1eed30f2edf3f5e56
Brian Coria <brian_coria@outlook.com>
2019-12-16 12:31:43 -0400

    dia_11 vimos communication y emmit

---
+++ dia_11_vue_cli/communication_/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <component-padre/>
+  </div>
+</template>
+
+<script>
+import componentPadre from './components/Padre.vue'
+
+export default {
+  name: 'app',
+  components: {
+    componentPadre
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ dia_11_vue_cli/communication_/src/components/Hijo.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="bg-light">
+      <p>Nombre: {{mensaje}} Apellido: {{apellido}} Edad: {{edad}}</p>
+       
+      <br>
+      <br>
+      
+       <button @click="$emit('cad', cadena )">Botoncito</button>
+       <ul  >
+           <li v-for="(l, index ) in mensaje1" :key="index" > 
+               <h1>{{l.titulo}}</h1>
+               <br>
+               <img :src="l.portada"/>
+               <br>
+               
+               <br>
+               
+               
+               
+           </li>
+       </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    
+    props: 
+        ['mensaje','apellido','edad','mensaje1','coches'],
+        data() {
+            return {
+            cadena:'componente hijo',
+            lista_coches: [
+                            {
+                            modelo: "Mustang",
+                            año:2012
+                            },
+                            {
+                            modelo: "Camaro",
+                            año:2011
+                            },
+                            {
+                            modelo: "Challenge",
+                            año:2010
+                            },
+                            {
+                            modelo: "Audi",
+                            año:2019
+                            }
+                            , {
+                            modelo: "BMW",
+                            año:2018
+                            }
+                        ]
+            }
+        },
+        mounted() {
+            this.$emit('cad', this.cadena)
+            this.$emit('coche', this.lista_coches)
+        },
+    
+}
+</script>
+
+<style>
+
+</style>
